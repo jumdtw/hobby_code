75fe1d55e1874136e113055c5514d23cfb6ba98b
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2018-12-07 21:09:36 +0900

    write soucre

--- 2-webserver/app.js
+++ 2-webserver/app.js
@@ -1,1 +1,11 @@
 // サーバーサイドのJavaScriptを書く
+const express = require('express');
+const app = express();
+const httpserver = require('http').Server(app);
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+httpserver.listen(PORT, () => {
+  console.log(`ポート ${PORT} で待機中`);
+});
